8e0fab1328e1e3b9e68ad6c76a2de6a16cd92dee
nssortega31-ui <nssortega31@src.pshs.edu.ph>
2026-08-10 09:47:29 +0800

    Update Q1_Acacia_FA1_Ortega.py

--- Q1_Acacia_FA1_Ortega.py
+++ Q1_Acacia_FA1_Ortega.py
@@ -1,7 +1,15 @@
 import math
-p1 = int(input("Please give point 1's x value"))
-q1 = int(input("Please give point 1's y value"))
-p2 = int(input("Please give point 2's x value"))
-q2 = int(input("Please give point 2's y value"))
+
+# Ask user for the coordinates of the first point.
+x1point = int(input("Please give point 1's x value"))
+y1point = int(input("Please give point 1's y value"))
+
+# Ask user for the coordinates of the second point.
+x2point = int(input("Please give point 2's x value"))
+y2point = int(input("Please give point 2's y value"))
+
+# Calculate the distance using sqrt() and pow().
 d = math.sqrt(math.pow(p2 - p1, 2) + math.pow(q2 - q1, 2))
-print("The distance between P1 and P2 is", d)
+
+# Output or give the Answer
+print("The distance between P1 and P2 is", d).
