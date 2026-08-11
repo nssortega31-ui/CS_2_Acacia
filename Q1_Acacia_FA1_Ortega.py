9ced1e43e3aae09d60da38096b2279a7b0b4a970
nssortega31-ui <nssortega31@src.pshs.edu.ph>
2026-08-11 09:15:22 +0800

    Update Q1_Acacia_FA1_Ortega.py

--- Q1_Acacia_FA1_Ortega.py
+++ Q1_Acacia_FA1_Ortega.py
@@ -8,7 +8,7 @@ y1point = int(input("Please give point 1's y value"))
 x2point = int(input("Please give point 2's x value"))
 y2point = int(input("Please give point 2's y value"))
 
-# Calculate the distance using sqrt() and pow().
+# Calculate the distance using the distance formula and sqrt() and pow().
 distance = math.sqrt(math.pow(x2point - x1point, 2) + math.pow(y2point - y1point, 2))
 
 # Output or give the Answer
